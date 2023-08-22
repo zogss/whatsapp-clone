4d5476315615d89d7f8f67052a0a4ae37a31bb45
zogss <yanlucasbranco@gmail.com>
2023-08-22 02:10:39 -0300

    server app setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome whatsapp-clone 👋
+              Welcome web 👋
             </h1>
           </div>
 
